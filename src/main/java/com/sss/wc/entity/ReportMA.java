c00167e3e112913a9b7de15d1b506dc4016b497c
Gayan Malshan <agulugaha@outlook.com>
2019-05-13 11:12:57 +0530

    created Report Entity for Management Assistants.

---
+++ src/main/java/com/sss/wc/entity/ReportMA.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sss.wc.entity;
+
+import java.util.List;
+/**
+ *
+ * @author Windhelm
+ */
+public class ReportMA {
+    
+    private String NIC;
+    private String officerName;
+    private int noOfPublicManagementAssistantsInSP;
+    private List<String> namesOfPublicManagementAssistantsWorksInSP;
+    private int noOfPublicManagementAssistantsRecruited;
+    private List<String> namesOfPublicManagementAssistantRecruited;
+    
+    private int noOfOfficersGalle;
+    private int noOfOfficersMatara;
+    private int noOfOfficersHambantota;
+
+    private int noOfClass3OfficersGalle;
+    private int noOfClass3OfficersMatara;
+    private int noOfClass3OfficersHambanthota;
+    private List<String>  namesOfClass3OfficersGalle;
+    private List<String>  namesOfClass3OfficersMatara;
+    private List<String>  namesOfClass3OfficersHambanthota;
+    
+    private int noOfClass2OfficersGalle;
+    private int noOfClass2OfficersMatara;
+    private int noOfClass2OfficersHambanthota;
+    private List<String>  namesOfClass2OfficersGalle;
+    private List<String>  namesOfClass2OfficersMatara;
+    private List<String>  namesOfClass2OfficersHambanthota;
+    
+    private int noOfClass1OfficersGalle;
+    private int noOfClass1OfficersMatara;
+    private int noOfClass1OfficersHambanthota;
+    private List<String>  namesOfClass1OfficersGalle;
+    private List<String>  namesOfClass1OfficersMatara;
+    private List<String>  namesOfClass1OfficersHambanthota;
+    
+    private List<String> NamesOfSpecialGradeOfficersGalle;
+    private List<String> NamesOfSpecialGradeOfficersMatara;
+    private List<String> NamesOfSpecialGradeOfficersHambanthota;
+    private int noOfSpecialGradeOfficersGalle;
+    private int noOfSpecialGradeOfficersMatara;
+    private int noOfSpecialGradeOfficersHambanthota;
+    
+    private InstitutionOfficerDetails instituteOfficerDetails;
+    private List<String> officersWhoAbsorbedService2004;
+    private List<String> officersWhoAbsorbedService2013;
+               
+    
+    
+                    
+}
